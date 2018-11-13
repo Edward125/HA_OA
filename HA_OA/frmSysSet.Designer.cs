9dd7550782bc2e3506b9327e9dae59896613f242
Edward <edward_song@yeah.net>
2018-11-13 10:24:46 +0800

    add version ok

--- HA_OA/frmSysSet.Designer.cs
+++ HA_OA/frmSysSet.Designer.cs
@@ -33,12 +33,21 @@
             this.tabBodySet = new System.Windows.Forms.TabPage();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabSysSet = new System.Windows.Forms.TabPage();
+            this.btnDeleteVersion = new System.Windows.Forms.Button();
+            this.btnAddVersion = new System.Windows.Forms.Button();
+            this.txtVersion = new System.Windows.Forms.TextBox();
+            this.lstVersion = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.tabMain.SuspendLayout();
             this.tabBodySet.SuspendLayout();
+            this.tabSysSet.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabMain
             // 
+            this.tabMain.Controls.Add(this.tabSysSet);
             this.tabMain.Controls.Add(this.tabUserSet);
             this.tabMain.Controls.Add(this.tabBodySet);
             this.tabMain.Location = new System.Drawing.Point(12, 12);
@@ -46,6 +55,7 @@
             this.tabMain.SelectedIndex = 0;
             this.tabMain.Size = new System.Drawing.Size(940, 437);
             this.tabMain.TabIndex = 0;
+            this.tabMain.SelectedIndexChanged += new System.EventHandler(this.tabMain_SelectedIndexChanged);
             // 
             // tabUserSet
             // 
@@ -86,6 +96,76 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "执法仪型号管理";
             // 
+            // tabSysSet
+            // 
+            this.tabSysSet.Controls.Add(this.label2);
+            this.tabSysSet.Controls.Add(this.label1);
+            this.tabSysSet.Controls.Add(this.btnDeleteVersion);
+            this.tabSysSet.Controls.Add(this.btnAddVersion);
+            this.tabSysSet.Controls.Add(this.txtVersion);
+            this.tabSysSet.Controls.Add(this.lstVersion);
+            this.tabSysSet.Location = new System.Drawing.Point(4, 22);
+            this.tabSysSet.Name = "tabSysSet";
+            this.tabSysSet.Size = new System.Drawing.Size(932, 411);
+            this.tabSysSet.TabIndex = 2;
+            this.tabSysSet.Text = "系统设置";
+            this.tabSysSet.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteVersion
+            // 
+            this.btnDeleteVersion.Location = new System.Drawing.Point(158, 121);
+            this.btnDeleteVersion.Name = "btnDeleteVersion";
+            this.btnDeleteVersion.Size = new System.Drawing.Size(108, 32);
+            this.btnDeleteVersion.TabIndex = 7;
+            this.btnDeleteVersion.Text = "删除版本号";
+            this.btnDeleteVersion.UseVisualStyleBackColor = true;
+            // 
+            // btnAddVersion
+            // 
+            this.btnAddVersion.Location = new System.Drawing.Point(158, 83);
+            this.btnAddVersion.Name = "btnAddVersion";
+            this.btnAddVersion.Size = new System.Drawing.Size(108, 32);
+            this.btnAddVersion.TabIndex = 6;
+            this.btnAddVersion.Text = "添加版本号";
+            this.btnAddVersion.UseVisualStyleBackColor = true;
+            this.btnAddVersion.Click += new System.EventHandler(this.btnAddVersion_Click);
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(158, 56);
+            this.txtVersion.Name = "txtVersion";
+            this.txtVersion.Size = new System.Drawing.Size(108, 21);
+            this.txtVersion.TabIndex = 5;
+            this.txtVersion.TextChanged += new System.EventHandler(this.txtVersion_TextChanged);
+            // 
+            // lstVersion
+            // 
+            this.lstVersion.FormattingEnabled = true;
+            this.lstVersion.ItemHeight = 12;
+            this.lstVersion.Location = new System.Drawing.Point(15, 35);
+            this.lstVersion.Name = "lstVersion";
+            this.lstVersion.Size = new System.Drawing.Size(137, 124);
+            this.lstVersion.TabIndex = 4;
+            this.lstVersion.SelectedIndexChanged += new System.EventHandler(this.lstVersion_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "当前可用版本号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "新版本号";
+            // 
             // frmSysSet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -94,8 +174,11 @@
             this.Controls.Add(this.tabMain);
             this.Name = "frmSysSet";
             this.Text = "frmSysSet";
+            this.Load += new System.EventHandler(this.frmSysSet_Load);
             this.tabMain.ResumeLayout(false);
             this.tabBodySet.ResumeLayout(false);
+            this.tabSysSet.ResumeLayout(false);
+            this.tabSysSet.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -107,5 +190,12 @@
         private System.Windows.Forms.TabPage tabBodySet;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabPage tabSysSet;
+        private System.Windows.Forms.Button btnDeleteVersion;
+        private System.Windows.Forms.Button btnAddVersion;
+        private System.Windows.Forms.TextBox txtVersion;
+        private System.Windows.Forms.ListBox lstVersion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- HA_OA/frmSysSet.cs
+++ HA_OA/frmSysSet.cs
@@ -15,5 +15,88 @@ namespace HA_OA
         {
             InitializeComponent();
         }
+
+
+        /// <summary>
+        /// load version
+        /// </summary>
+        private void LoadVersion()
+        {
+            lstVersion.Items.Clear();
+            string sql = "select version  from ha_oa_version where isrun = 1";
+
+            List<string> listversion = new List<string>();
+            p.QueryDatabase(p.ConnStr, sql, "version", out listversion);
+            if (listversion.Count > 0)
+            {
+                foreach (string item in listversion )
+                {
+                    lstVersion.Items.Add(item);
+                }
+            }
+
+        }
+
+        private void tabMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabSysSet.Focused)
+            {
+                LoadVersion();
+                btnAddVersion.Enabled = false;
+                btnDeleteVersion.Enabled = false;
+            }
+        }
+
+        private void frmSysSet_Load(object sender, EventArgs e)
+        {
+            LoadVersion();
+            btnAddVersion.Enabled = false;
+            btnDeleteVersion.Enabled = false;
+        }
+
+        private void lstVersion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstVersion.SelectedIndex != -1)
+                btnDeleteVersion.Enabled = true;
+        }
+
+        private void txtVersion_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty (txtVersion.Text.Trim ()))
+                btnAddVersion.Enabled = true ;
+
+        }
+
+        private void btnAddVersion_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("是否添加新的版本号:" + txtVersion.Text.Trim ()+",添加后,该版本的软件可以运行.添加点击是(Y),不添加点击否(N)?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                string exmsg = string.Empty;
+
+                string sql = "insert into ha_oa_version () values ('" + txtVersion.Text.Trim() + "',1)";
+                if (p.InsertDate2Database(p.ConnStr, sql, out exmsg))
+                {
+                    MessageBox.Show("更新数据库成功.", "Update Database Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtVersion.Text = string.Empty;
+                    LoadVersion();
+                }
+                else
+                {
+                    MessageBox.Show("更新数据库失败," + exmsg, "Update Database Fail", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    txtVersion.SelectAll();
+                    txtVersion.Focus();
+                }
+
+                
+                
+
+               
+            }
+            
+
+
+
+        }
     }
 }
